0815b4d73f646c5ca5519499aa6bb485b5bd3f16
L1keava <3288922513@qq.com>
2023-10-11 16:30:32 +0800

    test

--- src/bigIntegerDemo.java
+++ src/bigIntegerDemo.java
@@ -1,10 +1,11 @@
 import java.math.BigInteger;
-public class javaC_189 {
+public class bigIntegerDemo {
     public static void main(String[] args){
         BigInteger result = BigInteger.valueOf(0);
         for (long i=1;i<=20230408;i++){
             result = result.add(BigInteger.valueOf(i));
         }
+        int a  =1;
         System.out.println(result);
     }
 }
