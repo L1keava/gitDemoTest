fd963915a2b8f80a6a81cec68c5bb3da3db34260
L1keava <3288922513@qq.com>
2023-10-11 16:10:36 +0800

    百钱买百鸡经典算法

--- src/Scy26_zy10.java
+++ src/Scy26_zy10.java
@@ -1,5 +1,5 @@
 public class Scy26_zy10 {
     public static void main(String[] args){
-        
+
     }
 }
--- src/javaC_189.java
+++ src/javaC_189.java
@@ -1,6 +1,10 @@
 import java.math.BigInteger;
-public class test01 {
+public class javaC_189 {
     public static void main(String[] args){
-        
+        BigInteger result = BigInteger.valueOf(0);
+        for (long i=1;i<=20230408;i++){
+            result = result.add(BigInteger.valueOf(i));
+        }
+        System.out.println(result);
     }
 }
