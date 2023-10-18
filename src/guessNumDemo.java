abca4cd9c55b8f28c123539f35f7cda2d5f22bce
L1keava <3288922513@qq.com>
2023-10-18 23:37:32 +0800

    优化代码

--- src/guessNumDemo.java
+++ src/guessNumDemo.java
@@ -1,94 +1,20 @@
 import java.util.*;
+
 public class guessNumDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         int num = random.nextInt(11);
-        int times = 0;
-        int input;
-        ++times;
-        System.out.println("请输入一个整数：");
-        input = scanner.nextInt();
-        if (input==num){
-            System.out.println("恭喜，猜对了！");
-            System.out.println("你用了 " + times + " 次机会");
-        }else {
-            if (input > num) {
+        for (int times = 1; times <= 3; times++) {
+            System.out.println("请输入整数：");
+            int in = scanner.nextInt();
+            if (num < in) {
                 System.out.println("猜大了");
-                ++times;
-                System.out.println("请输入一个整数：");
-                input = scanner.nextInt();
-                if (input==num){
-                    System.out.println("恭喜，猜对了！");
-                    System.out.println("你用了 " + times + " 次机会");
-                }else {
-                    if (input > num) {
-                        System.out.println("猜大了");
-                        ++times;
-                        System.out.println("请输入一个整数：");
-                        input = scanner.nextInt();
-                        if (input==num){
-                            System.out.println("恭喜，猜对了！");
-                            System.out.println("你用了 " + times + " 次机会");
-                        }else {
-                            System.out.println("三次机会用完");
-                            System.out.println("正确答案是：" + num);
-                        }
-                    } else {
-                        System.out.println("猜小了");
-                        ++times;
-                        System.out.println("请输入一个整数：");
-                        input = scanner.nextInt();
-                        if (input==num){
-                            System.out.println("恭喜，猜对了！");
-                            System.out.println("你用了 " + times + " 次机会");
-                        }else {
-                            if (input > num) {
-                                System.out.println("猜大了");
-                            } else {
-                                System.out.println("猜小了");
-                            }
-                        }
-                    }
-                }
-            } else {
+            } else if (num > in) {
                 System.out.println("猜小了");
-                ++times;
-                System.out.println("请输入一个整数：");
-                input = scanner.nextInt();
-                if (input==num){
-                    System.out.println("恭喜，猜对了！");
-                    System.out.println("你用了 " + times + " 次机会");
-                }else {
-                    if (input > num) {
-                        System.out.println("猜大了");
-                        ++times;
-                        System.out.println("请输入一个整数：");
-                        input = scanner.nextInt();
-                        if (input==num){
-                            System.out.println("恭喜，猜对了！");
-                            System.out.println("你用了 " + times + " 次机会");
-                        }else {
-                            System.out.println("三次机会用完");
-                            System.out.println("正确答案是：" + num);
-                        }
-                    } else {
-                        System.out.println("猜小了");
-                        ++times;
-                        System.out.println("请输入一个整数：");
-                        input = scanner.nextInt();
-                        if (input==num){
-                            System.out.println("恭喜，猜对了！");
-                            System.out.println("你用了 " + times + " 次机会");
-                        }else {
-                            System.out.println("三次机会用完");
-                            System.out.println("正确答案是：" + num);
-                        }
-                    }
-                }
+            } else {
+                System.out.println("猜对了！你用了 " + times + " 次机会");
             }
         }
     }
 }
-
-
