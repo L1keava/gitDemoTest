41e5719ffb188bd9182427232a6b9339f921f58d
L1keava <3288922513@qq.com>
2023-11-08 14:32:46 +0800

    merge

--- src/arrayDemo.java
+++ src/arrayDemo.java
@@ -4,63 +4,67 @@ import java.util.Scanner;
 public class arrayDemo {
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
-    public static String[] arg = new String[1];
     public static int[] array = new int[10];
     public static int[] array2 = new int[10];
 
     public static void main(String[] args) {
+        int target;
+        while (true) {
+            System.out.println("""
+                    数组应用Demo 功能如下\s
+                     1、为数组生成随机数  2、为数组输入数值    3、数组冒泡排序\s
+                     4、数组选择排序     5、输出数组         6、数组元素求和
+                     7、数组元素求平均值  8、数组删除元素     9、插入元素并排序
+                     10、数组颠倒输出    11、数组复制元素    12、结束程序""");
+            System.out.println("请输入你的选择：");
 
-        System.out.println("""
-                数组应用Demo 功能如下\s
-                 1、为数组生成随机数  2、为数组输入数值    3、数组冒泡排序\s
-                 4、数组选择排序     5、输出数组         6、数组元素求和
-                 7、数组元素求平均值  8、数组删除元素     9、数组插入元素
-                 10、数组颠倒输出    11、数组复制元素    12、结束程序""");
-        System.out.println("请输入你的选择：");
-
-        int choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("请输入随机数最大值；");
-                int bounds = scanner.nextInt();
-                arrayGenRandom(array, bounds);
-                break;
-            case 2:
-                arrayInput(array);
-                break;
-            case 3:
-                bubbleSort(array);
-                break;
-            case 4:
-                selectionSort(array);
-                break;
-            case 5:
-                printArray(array);
-                break;
-            case 6:
-                System.out.println("数组元素和为：" + arraySum(array));
-                break;
-            case 7:
-                System.out.println("数组元素平均值为：" + arrayAvg(array));
-                break;
-            case 8:
-                arrayRemove(array);
-                break;
-            case 9:
-                arrayInsert(array);
-                break;
-            case 10:
-                arrayReverse(array);
-                break;
-            case 11:
-                arrayCopy(array, array2);
-                break;
-            case 12:
-                end();
-                break;
-            default:
-                System.out.println("错误选项，请正确输入！");
-                main(arg);
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("请输入随机数最大值；");
+                    target = scanner.nextInt();
+                    arrayGenRandom(array, target);
+                    break;
+                case 2:
+                    arrayInput(array);
+                    break;
+                case 3:
+                    bubbleSort(array);
+                    break;
+                case 4:
+                    selectionSort(array);
+                    break;
+                case 5:
+                    printArray(array,array.length);
+                    break;
+                case 6:
+                    System.out.println("数组元素和为：" + arraySum(array));
+                    break;
+                case 7:
+                    System.out.println("数组元素平均值为：" + arrayAvg(array));
+                    break;
+                case 8:
+                    System.out.println("请输入你要删除的数字：");
+                    target = scanner.nextInt();
+                    arrayRemove(array,target);
+                    break;
+                case 9:
+                    System.out.println("请输入想要插入的整数：" );
+                    target = scanner.nextInt();
+                    arrayInsert(array,target);
+                    break;
+                case 10:
+                    arrayReverse(array);
+                    break;
+                case 11:
+                    arrayCopy(array, array2);
+                    break;
+                case 12:
+                    end();
+                    break;
+                default:
+                    System.out.println("错误选项，请正确输入！");
+            }
         }
     }
 
@@ -70,7 +74,6 @@ public class arrayDemo {
             array[i] = random.nextInt(bounds);
         }
         System.out.println("成功生成随机数！");
-        main(arg);
     }
 
     public static void arrayInput(int[] array) {
@@ -80,7 +83,6 @@ public class arrayDemo {
             array[i] = scanner.nextInt();
         }
         System.out.println("输入完成！");
-        main(arg);
     }
 
     public static void bubbleSort(int[] array) {
@@ -111,8 +113,7 @@ public class arrayDemo {
                 }
                 break;
         }
-        printArray(array);
-        main(arg);
+        printArray(array,array.length);
     }
 
     public static void selectionSort(int[] array) {
@@ -143,17 +144,15 @@ public class arrayDemo {
                 }
                 break;
         }
-        printArray(array);
-        main(arg);
+        printArray(array,array.length);
     }
 
-    public static void printArray(int[] array) {
+    public static void printArray(int[] array,int index) {
         //输出数组
-        for (int j : array) {
-            System.out.print(j + "  ");
+        for (int i = 0; i < index; i++) {
+            System.out.print(array[i] + "  ");
         }
         System.out.println();
-        main(arg);
     }
 
     public static int arraySum(int[] array) {
@@ -170,12 +169,32 @@ public class arrayDemo {
         return (double) arraySum(array) / array.length;
     }
 
-    public static void arrayRemove(int[] array) {
-        //数组删除元素W
+    public static void arrayRemove(int[] array,int target) {
+        //数组删除元素
+        for (int i = 0;i<array.length;i++){
+            if (target == array[i]){
+                for (int j = i;j<array.length-1;j++){
+                    array[j] = array[j+1];
+                }
+                printArray(array,array.length-1);
+            }else {
+                System.out.println("未找到：" + target);
+            }
+        }
     }
 
-    public static void arrayInsert(int[] array) {
+    public static void arrayInsert (int[] array,int target) {
         //数组插入元素
+        int[] newArray = new int[11];
+        System.arraycopy(array,0,newArray,0,array.length);
+        System.out.println("请选择排序方式： \n 1、升序\n 2、降序");
+        int choice = scanner.nextInt();
+        switch (choice){
+            case 1:
+                for (int i = 0;i<newArray.length;i++){
+                    
+                }
+        }
     }
 
     public static void arrayReverse(int[] array) {
@@ -192,7 +211,7 @@ public class arrayDemo {
         }
          */
         System.out.println("已复制到array2:");
-        printArray(array2);
+        printArray(array2,array2.length);
     }
 
     public static void end() {
