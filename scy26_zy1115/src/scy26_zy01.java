d84ea211f5aa34ded48cab2b793761352aaceef6
L1keava <3288922513@qq.com>
2023-11-22 17:45:38 +0800

    1121作业完成 1115作业fix

--- scy26_zy1115/src/scy26_zy01.java
+++ scy26_zy1115/src/scy26_zy01.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 
 class yuan {
-    double r;
+    private double r;
 
+    void setR(double r){
+        this.r = r;
+    }
+
+    double getR(){
+        return r;
+    }
     double area() {
-        return 3.14 * r * r;
+        return 3.14 * getR() * getR();
     }
 
     double zc() {
-        return 2 * 3.14 * r;
+        return 2 * 3.14 * getR();
     }
 
     void printf() {
-        System.out.println("半径为：" + r + ",面积为：" + area() + ",周长为：" + zc());
+        System.out.println("半径为：" + getR() + ",面积为：" + area() + ",周长为：" + zc());
     }
 }
 
@@ -22,7 +29,7 @@ public class scy26_zy01 {
         yuan y1 = new yuan();
         System.out.println("请输入10个圆的半径：");
         for (int i = 0; i < 10; i++) {
-            y1.r = scanner.nextDouble();
+            y1.setR(scanner.nextDouble());
             y1.printf();
         }
     }
--- scy26_zy1121/src/scy26_zy07.java
+++ scy26_zy1121/src/scy26_zy07.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
+class book {
+    protected String name, id, press, price;
+
+    public book(String name, String id, String press, String price) {
+        this.name = name;
+        this.id = id;
+        this.press = press;
+        this.price = price;
+    }
+
+    void get() {
+        System.out.println("书名：" + name + " 书号：" + id + " 出版社：" + press + " 价格：" + price);
+    }
+
+}
+
 public class scy26_zy07 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入书名 书号 出版社 价格：");
+        book book = new book(scanner.next(), scanner.next(), scanner.next(), scanner.next());
+        book.get();
     }
 }
