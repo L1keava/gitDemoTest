cdbc27d86c98e645843e23fb580557ab18cb4f15
L1keava <3288922513@qq.com>
2023-11-29 17:08:34 +0800

    第二题完成

--- scy26_zy1129/src/scy26_zy03.java
+++ scy26_zy1129/src/scy26_zy03.java
@@ -1,3 +1,14 @@
+class yuan {
+    double r;
+    private final double pi = 3.14;
+    double getArea(){
+        return pi*r*r;
+    }
+    public void setR(double r) {
+        this.r = r;
+    }
+}
+
 public class scy26_zy03 {
     public static void main(String[] args) {
 
