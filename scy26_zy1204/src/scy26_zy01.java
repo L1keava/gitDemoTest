1540bf224a8e0d109c5608885f5f8cfc175331c6
L1keava <3288922513@qq.com>
2023-12-04 19:46:40 +0800

    1204作业前两题完成

--- scy26_zy1204/src/scy26_zy01.java
+++ scy26_zy1204/src/scy26_zy01.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class scy26_zy01 {
+import java.util.Scanner;
+
+abstract class yuan {
+    double r;
+    final double pi = 3.14;
+
+    abstract double area(double r);
+}
+
+class yz extends yuan {
+    double r, h;
+    final double pi = 3.14;
+
+    double area(double r) {
+        return pi * r * r;
+    }
+
+    double tj(double r, double h) {
+        return area(r) * h;
+    }
+
+    @Override
+    public String toString() {
+        return "体积为：" + tj(r, h);
+    }
+}
+
+public class scy26_zy01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        yz yz1 = new yz();
+        System.out.println("请输入圆柱体的半径和高：");
+        yz1.r = scanner.nextDouble();
+        yz1.h = scanner.nextDouble();
+        System.out.println(yz1);
+    }
 }
--- scy26_zy1204/src/scy26_zy02.java
+++ scy26_zy1204/src/scy26_zy02.java
@@ -1,5 +1,47 @@
-public class scy26_zy01 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+abstract class xzs {
+    double a, b, c;
+
+    abstract double getPerimeter(double a, double b, double c);
+
+    abstract double getArea(double a, double b, double c);
+}
+
+class sjx extends xzs {
+    double a, b, c, h;
+
+    public sjx(double a, double b, double c, double h) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.h = h;
+    }
+
+    double getPerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    double getArea(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+
+    double tj(double a, double b, double c, double h) {
+        return getArea(a, b, c) * h;
+    }
+
+    String info() {
+        return "周长为：" + getPerimeter(a, b, c) + "，面积为：" + getArea(a, b, c);
+    }
+
+}
+
+public class scy26_zy02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入三角形的三条边以及高：");
+        sjx sjx1 = new sjx(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
+        System.out.println(sjx1.info());
     }
 }
