3bd85da2c368fb4ea67aa87775d95d30151d649a
L1keava <example@qq.com>
2023-12-27 15:43:35 +0800

    new

--- scy26_zy1019/src/scy26_zy14_for.java
+++
@@ -1,9 +0,0 @@
-public class scy26_zy14_for {
-    public static void main(String[] args) {
-        int num, result = 1;
-        for (num = 1; num <= 10; num++) {
-            result *= num;
-        }
-        System.out.println("结果是：" + result);
-    }
-}
--- scy26_zy1019/src/scy26_zy18_for.java
+++
@@ -1,9 +0,0 @@
-public class scy26_zy18_for {
-    public static void main(String[] args) {
-        double num, sum = 0;
-        for (num = 1; num <= 10; num++) {
-            sum += 1 / num;
-        }
-        System.out.println("结果是：" + sum);
-    }
-}
