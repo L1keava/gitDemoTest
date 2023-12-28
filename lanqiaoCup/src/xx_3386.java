c0cdfc6cfc3bdb812f6421e64b05789b4e2523de
L1keava <3288922513@qq.com>
2023-12-28 10:17:22 +0800

    commit

--- lanqiaoCup/src/xx_3386.java
+++ lanqiaoCup/src/xx_3386.java
@@ -6,24 +6,21 @@ public class xx_3386 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         //在此输入您的代码...
-        param[] paramArr = new param[999];
-        String input = scan.next();
-        if (input.startsWith("PUT")){
+        param[] paramArr = new param[61234];
+        String input = scan.nextLine();
+        if (input.startsWith("PUT")) {
             //PUT MODE
-            String mode,msgName,msg;
-            int prior;
-            System.out.println("此时的input内容为：" + input);
-            input = input.replace("PUT ","");
-
-            mode = "PUT";
-            msgName = input.substring(0,input.indexOf(" ")+1);
-            input = input.replace(msgName+" ","");
-            msg = input.substring(0,input.indexOf(" ")+1);
-
-            prior = Integer.valueOf(input.replace(msg+" ",""));
-            paramArr[0] = new param(mode,msgName,msg,prior);
-            System.out.println(paramArr[0].toString());
-        }else if (input.startsWith("GET")){
+            String mode = "PUT", msgName;
+            int msg, prior;
+            input = input.replace("PUT ", "");
+            msgName = input.substring(0, input.indexOf(" "));
+            input = input.replace(msgName + " ", "");
+            msg = Integer.parseInt(input.substring(0, input.indexOf(" ")));
+            input = input.replace(msg + " ", "");
+            prior = Integer.parseInt(input);
+            paramArr[0] = new param(mode, msgName, msg, prior);
+            System.out.println(paramArr[0]);
+        } else if (input.startsWith("GET")) {
             //GET MODE
 
 
@@ -31,12 +28,14 @@ public class xx_3386 {
         scan.close();
     }
 }
-class param{
+
+class param {
     String mode;
     String msgName;
-    String msg;
+    int msg;
     int priority;
-    public param(String mode,String msgName,String msg,int priority){
+
+    public param(String mode, String msgName, int msg, int priority) {
         this.mode = mode;
         this.msgName = msgName;
         this.msg = msg;
@@ -45,6 +44,6 @@ class param{
 
     @Override
     public String toString() {
-        return "模式：" + mode +"消息名称：" + msgName +"消息内容：" + msg +"消息队列：" + priority;
+        return "模式：" + mode + "\n消息名称：" + msgName + "\n消息内容：" + msg + "\n消息队列：" + priority;
     }
 }
