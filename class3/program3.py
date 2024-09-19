a1b31c2a205c344f3b327aeb110fa68c137f63a3
L1keava <3288922513@qq.com>
2024-09-19 09:02:46 +0800

    no desc

--- class3/program3.py
+++ class3/program3.py
@@ -3,6 +3,6 @@ while i in range(1, 8):
     j = 1
     while j in range(1, i + 1):
         print("*", end="")
-        j+=1
+        j += 1
     print()
-    i+=1
+    i += 1
--- class3/program5.py
+++ class3/program5.py
@@ -1,6 +1,4 @@
-i = 1
-while i <= 9:
-    for j in range(1,i+1):
-        print("%d*%d=%d"%(j,i,i*j),end=" ")
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print("%d*%d=%d" % (j, i, i * j), end=" ")
     print()
-    i+=1
