51e24ae343fc71654ec6fff20f984d0c8753477f
L1keava <3288922513@qq.com>
2025-02-17 17:18:07 +0800

    no desc

---
+++ mybatisDemo/src/test/java/Test/UserTest.java
@@ -0,0 +1,26 @@
+package Test;
+
+import com.itheima.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+import java.io.IOException;
+import java.io.Reader;
+
+public class UserTest {
+    @Test
+    public void userFindByIdTest(){
+        String resources = "mybatis-config.xml";
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader(resources);
+        } catch (IOException e) {e.printStackTrace();}
+        SqlSessionFactory sqlMapper = new SqlSessionFactoryBuilder().build(reader);
+        SqlSession session = sqlMapper.openSession();
+        User user = session.selectOne("findById",1);
+        System.out.println(user.getUage());
+        session.close();
+    }
+}
