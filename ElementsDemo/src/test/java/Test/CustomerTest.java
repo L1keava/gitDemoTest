8d10d02e99e2d1ac882a72bd75d39153bbdca2d0
L1keava <3288922513@qq.com>
2025-03-10 17:04:51 +0800

    no desc

--- ElementsDemo/src/test/java/Test/CustomerTest.java
+++ ElementsDemo/src/test/java/Test/CustomerTest.java
@@ -30,14 +30,59 @@ public class CustomerTest {
     }
     
     @Test
-    public void findCustomerByNameAndJobs(){
+    public void findByNameAndJobs(){
         Customer find_conditions = new Customer();
         find_conditions.setUsername("jack");
         find_conditions.setJobs("teacher");
         List<Customer> result = sqlSession.selectList("mapper.CustomerMap.findByNameAndJobs",find_conditions);
-        System.out.println("查找客户：");
+        System.out.println("根据姓名和职业查找：");
         for (Customer customer : result){
             System.out.println(customer);   
+        }
+        findByNameOrJobs();
+    }
+    public void findByNameOrJobs(){
+        Customer find_conditions = new Customer();
+        find_conditions.setUsername("tom");
+        find_conditions.setJobs("teacher");
+        List<Customer> result = sqlSession.selectList("mapper.CustomerMap.findByNameOrJobs",find_conditions);
+        System.out.println("根据姓名或职业查找：");
+        for (Customer customer : result){
+            System.out.println(customer);
+        }
+        findByWhere();
+    }
+    public void findByWhere(){
+        Customer find_conditions = new Customer();
+        find_conditions.setUsername("jack");
+        find_conditions.setJobs("teacher");
+        List<Customer> result = sqlSession.selectList("mapper.CustomerMap.findByWhere",find_conditions);
+        System.out.println("使用Where标签查找：");
+        for (Customer customer : result){
+            System.out.println(customer);
+        }
+        findByTrim();
+    }
+    public void findByTrim(){
+        Customer find_conditions = new Customer();
+        find_conditions.setUsername("tom");
+        find_conditions.setJobs("worker");
+        List<Customer> result = sqlSession.selectList("mapper.CustomerMap.findByWhere",find_conditions);
+        System.out.println("使用Trim查找：");
+        for (Customer customer : result){
+            System.out.println(customer);
+        }
+        updateBySet();
+    }
+    public void updateBySet(){
+        Customer update = new Customer();
+        update.setId(3);
+        update.setPhone("155555555555");
+        int rows = sqlSession.update("mapper.CustomerMap.updateBySet",update);
+        if (rows > 0){
+            System.out.println("修改了" + rows + "条数据。");
+        }else {
+            System.out.println("0条记录被修改。");
         }
     }
     @After
